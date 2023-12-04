735bdf4ea03f1bd4892799a0021c50fff896f5f8
Andres Astra <andieroid@arizona.edu>
2023-12-04 12:47:39 -0700

    Commit
    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from "./app.component";
-import { LoginComponent } from "./login/login.component";
+import { WindowCenterComponent } from "./window-center/window-center.component";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: "full" },
-  { path: "login", component: LoginComponent },
-  {path: "main", component: AppComponent}
+  { path: "login", component: AuthComponent },
+  {path: "main", component: WindowCenterComponent},
 ];
 
 @NgModule({
