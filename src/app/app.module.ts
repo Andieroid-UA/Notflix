5e912af9309cfc77a9c092b0f98145be1abe1a1c
Andres Astra <andieroid@arizona.edu>
2023-12-07 19:56:52 -0700

    Now can delete folders
    Saving before I wreck this again...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { WindowCenterComponent } from './window-center/window-center.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
@@ -11,15 +10,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { FolderService } from './sidebar/folder.service';
+import { ModalComponent } from './modal/modal.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    NavbarComponent,
     WindowCenterComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    ModalComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,39 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
+import { AlertComponent } from '../alert/alert.component';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-
 export class SidebarComponent {
   folders = [
     { name: 'Trials', count: 0 },
     { name: 'All', count: 1 },
-    // ... other folders
   ];
 
-  newFolderName: string = ''; // Ensure 'newFolderName' is declared as a string property
+  newFolderName: string = '';
+  errorMessage: string = '';
 
-  constructor() {
-    // Example of updating a specific folder's count in the constructor
-    const folderToUpdate = this.folders.find(folder => folder.name === 'Trials');
-    if (folderToUpdate) {
-      folderToUpdate.count += 1; // Update the count as needed
-    }
-  }
+  showDeleteModal = false;
 
-  folderClicked(folder: { name: string, count: number }): void {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private viewContainerRef: ViewContainerRef) {}
+
+  folderClicked(folder: { name: string; count: number }): void {
     // Handle folder click functionality here
     console.log(`Clicked folder: ${folder.name}`);
     // Potentially, update folder counts or perform other actions
   }
 
   addNewFolder(folderName: string): void {
-    // Logic to add a new folder
-    const newFolder = { name: folderName, count: 0 };
-    this.folders.push(newFolder);
-    // Potentially, perform actions after adding the folder
+    if (folderName.trim() !== '') {
+      const newFolder = { name: folderName, count: 0 };
+      this.folders.push(newFolder);
+      this.errorMessage = ''; // Clear error message if folder is added
+      // Potentially, perform actions after adding the folder
+    } else {
+      this.errorMessage = 'Folder name cannot be empty'; // Set error message
+    }
+  }
+
+  deleteFolderClicked(folder: { name: string; count: number }): void {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const componentRef = this.viewContainerRef.createComponent(alertComponentFactory);
+
+    componentRef.instance.message = `Are you sure you want to delete folder "${folder.name}"?`;
+    componentRef.instance.selectedFolder = folder;
+
+    componentRef.instance.deleteFolder.subscribe((folderToDelete: { name: string; count: number }) => {
+      const index = this.folders.findIndex(f => f === folderToDelete);
+      if (index !== -1) {
+        this.folders.splice(index, 1);
+        // Perform additional deletion actions or update UI
+      }
+      componentRef.destroy();
+    });
+
+    componentRef.instance.closeModal.subscribe(() => {
+      componentRef.destroy();
+    });
   }
 }
-
