01f6b851aaf369d72f8f8dafe73887ca730b788d
Andres Astra <andieroid@arizona.edu>
2023-12-11 14:49:32 -0700

    Button to Switch Dashboard and Tasklist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { WindowCenterComponent } from "./window_format/window-center/window-center.component";
 import { AuthComponent } from "./auth/auth.component";
+import { WindowDashboardComponent } from "./shared/window_format/window-dashboard/window-dashboard.component";
+import { WindowTasklistComponent } from "./shared/window_format/window-tasklist/window-tasklist.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/auth", pathMatch: "full" },
   { path: "auth", component: AuthComponent },
-  {path: "main", component: WindowCenterComponent},
+  { path: "tasklist", component: WindowTasklistComponent},
+  { path: "dashboard", component: WindowDashboardComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './window_format/sidebar/sidebar.component';
-import { NavbarComponent } from './window_format/navbar/navbar.component';
-import { WindowCenterComponent } from './window_format/window-center/window-center.component';
+import { SidebarComponent } from './shared/window_format/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/window_format/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { FolderService } from './window_format/sidebar/folder.service';
+import { FolderService } from './shared/window_format/sidebar/folder.service';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { TaskComponent } from './task/task/task.component';
-import { TaskDialogComponent } from './task/task-dialog/task-dialog.component';
-import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskComponent } from './pages/task/task/task.component';
+import { TaskDialogComponent } from './pages/task/task-dialog/task-dialog.component';
+import { TaskListComponent } from './pages/task/task-list/task-list.component';
 import { AlertComponent } from './alert/alert.component';
 import { ModalComponent } from './modal/modal.component';
+import { WindowDashboardComponent } from './shared/window_format/window-dashboard/window-dashboard.component';
+import { WindowTasklistComponent } from './shared/window_format/window-tasklist/window-tasklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    WindowCenterComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     DashboardComponent,
@@ -32,6 +32,9 @@ import { ModalComponent } from './modal/modal.component';
     TaskListComponent,
     AlertComponent,
     ModalComponent,
+    WindowDashboardComponent,
+    WindowTasklistComponent
+
   ],
   imports: [
     BrowserModule,
