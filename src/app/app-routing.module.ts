bc975e499a9c6e7df864030f5ad1de057eb57b20
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-12-20 18:04:55 -0600

    Commiting Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AuthComponent } from "./auth/auth.component";
 import { CalendarComponent } from "./pages/calendar/calendar.component";
 import { WindowDashboardComponent } from "./pages/window-dashboard/window-dashboard.component";
 
+
 const appRoutes: Routes = [
   { path: "", redirectTo: "/auth", pathMatch: "full" },
   { path: 'auth', component: AuthComponent },
   { path: 'tasklist', component: CalendarComponent },
   { path: 'dashboard', component: WindowDashboardComponent },
+
 ];
 
 @NgModule({
